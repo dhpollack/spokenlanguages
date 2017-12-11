abeb39fc748ec15a8f14723aad84ec4dc2c97014
David Pollack <david@da3.net>
2017-12-11 19:22:58 +0100

    resnet101

--- models/resnet.py
+++ models/resnet.py
@@ -14,3 +14,16 @@ def resnet34(pretrained=False, num_langs=2, **kwargs):
     model = nn.Sequential(conv2d, resnet)
 
     return model
+
+def resnet101(pretrained=False, num_langs=2, **kwargs):
+
+    conv2d = nn.Conv2d(1, 3, 1) # turn 1 channel into 3 to simulate image
+    conv2d.weight.data[0] = 1. # ensure original spectrogram is maintained
+
+    resnet = model_zoo.resnet101(pretrained=pretrained, **kwargs)
+    # change the last fc layer
+    resnet.fc = nn.Linear(512 * 1, num_langs)
+
+    model = nn.Sequential(conv2d, resnet)
+
+    return model
